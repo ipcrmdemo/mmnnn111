4c307c776f319e3e2a85c3c2bb53465a551a5e5d
Matt Cadorette <mcadorette@gmail.com>
2018-11-29 09:29:09 -0500

    Initial commit from Atomist

---
+++ src/main/java/com/mmnnn111/Mmnnn111Application.java
@@ -0,0 +1,12 @@
+package com.mmnnn111;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mmnnn111Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mmnnn111Application.class, args);
+	}
+}
